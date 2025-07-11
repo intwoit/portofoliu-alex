a0cbd6bb929497171a6185237f756d7e74694b2b
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-11 07:45:17 +0300

    Updated BTC and ETH average prices and investment values

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const investments = [
-  { asset: "BTC", entries: [{ amount: 8905 / 67905, price: 67905 }] },
-  { asset: "ETH", entries: [{ amount: 21663 / 2771, price: 2771 }] },
+  { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
+  { asset: "ETH", entries: [{ amount: 22463 / 2892, price: 2892 }] },
   { asset: "ADA", entries: [{ amount: 199 / 1.52, price: 1.52 }] },
   { asset: "LINK", entries: [{ amount: 16380 / 17.57, price: 17.57 }] },
   { asset: "DOT", entries: [{ amount: 11307 / 7.40, price: 7.40 }] },
