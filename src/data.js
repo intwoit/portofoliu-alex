c92608940153db5ee4fe72c7666848dd1dc93788
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-15 00:35:29 +0300

    Updated ETH investment price

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 22463 / 2892, price: 2892 }] },
+  { asset: "ETH", entries: [{ amount: 22185 / 2892, price: 2892 }] },
   { asset: "ADA", entries: [{ amount: 199 / 1.52, price: 1.52 }] },
   { asset: "LINK", entries: [{ amount: 16380 / 17.57, price: 17.57 }] },
   { asset: "DOT", entries: [{ amount: 11307 / 7.40, price: 7.40 }] },
