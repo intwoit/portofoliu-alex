92fd5e146ea54310f75852f0ed743f7911516af8
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-16 23:37:41 +0300

    Update ETH, FET, LDO, REZ positions + adjusted cashForDips

--- src/data.js
+++ src/data.js
@@ -6,14 +6,14 @@ const investments = [
   { asset: "DOT", entries: [{ amount: 10057 / 8.23, price: 8.23 }] },
   { asset: "SOL", entries: [{ amount: 5827 / 186.56, price: 186.56 }] },
   { asset: "SEI", entries: [{ amount: 5390 / 0.4767, price: 0.4767 }] },
-  { asset: "FET", entries: [{ amount: 400 / 0.7368, price: 0.7368 }] },
-  { asset: "RNDR", entries: [{ amount: 300 / 3.82, price: 3.82 }] },
-  { asset: "LDO", entries: [{ amount: 400 / 0.8693, price: 0.8693 }] },
-  { asset: "GMX", entries: [{ amount: 400 / 12.93, price: 12.93 }] },
-  { asset: "STRK", entries: [{ amount: 300 / 0.1417, price: 0.1417 }] },
+  { asset: "FET", entries: [{ amount: 600 / 0.7469, price: 0.7469 }] },
+  { asset: "RNDR", entries: [{ amount: 500 / 3.88, price: 3.88 }] },
+  { asset: "LDO", entries: [{ amount: 600 / 0.8853, price: 0.8853 }] },
+  { asset: "GMX", entries: [{ amount: 600 / 12.98, price: 12.98 }] },
+  { asset: "STRK", entries: [{ amount: 500 / 0.14136, price: 0.14136 }] },
   { asset: "RDNT", entries: [{ amount: 200 / 0.02478, price: 0.02478 }] },
-  { asset: "REZ", entries: [{ amount: 380 / 0.01302, price: 0.01302 }] },
-  { asset: "ETHFI", entries: [{ amount: 300 / 1.2167, price: 1.2167 }] },
+  { asset: "REZ", entries: [{ amount: 580 / 0.01388, price: 0.01388 }] },
+  { asset: "ETHFI", entries: [{ amount: 500 / 1.23, price: 1.23 }] },
 ];
 
 export default investments;
