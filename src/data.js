fb881f42ccb1fd76579612088602de836f323ec1
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-25 01:05:36 +0300

    Update portofoliu și logică aplicație – 25 iulie 2025

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
   { asset: "ETH", entries: [{ amount: 21579 / 2963, price: 2963 }] },
-  { asset: "LINK", entries: [{ amount: 14900 / 17.425, price: 17.425 }] },
+  { asset: "LINK", entries: [{ amount: 16400 / 17.50, price: 17.50 }] },
   { asset: "SOL", entries: [{ amount: 7327 / 181.91, price: 181.91 }] },
-  { asset: "SEI", entries: [{ amount: 911.13 / 0.4761, price: 0.4761 }] },
-  { asset: "FET", entries: [{ amount: 900 / 0.7644, price: 0.7644 }] },
-  { asset: "RNDR", entries: [{ amount: 1458 / 4.057, price: 4.057 }] },
+  { asset: "SEI", entries: [{ amount: 1211 / 0.424, price: 0.424 }] },
+  { asset: "FET", entries: [{ amount: 1800 / 0.755, price: 0.755 }] },
+  { asset: "RNDR", entries: [{ amount: 2358 / 4.123, price: 4.123 }] },
   { asset: "LDO", entries: [{ amount: 945 / 1.1573, price: 1.1573 }] },
   { asset: "GMX", entries: [{ amount: 300 / 13.22, price: 13.22 }] },
-  { asset: "STRK", entries: [{ amount: 280.74 / 0.1443, price: 0.1443 }] },
+  { asset: "STRK", entries: [{ amount: 480 / 0.137, price: 0.137 }] },
   { asset: "RDNT", entries: [{ amount: 200 / 0.02478, price: 0.02478 }] },
   { asset: "REZ", entries: [{ amount: 1270 / 0.013497, price: 0.013497 }] },
-  { asset: "ETHFI", entries: [{ amount: 1300 / 1.29, price: 1.29 }] },
-  { asset: "PENDLE", entries: [{ amount: 2595 / 3.99, price: 3.99 }] },
+  { asset: "ETHFI", entries: [{ amount: 2100 / 1.257, price: 1.257 }] },
+  { asset: "PENDLE", entries: [{ amount: 3495 / 4.106, price: 4.106 }] },
 ];
 
 export default investments;
