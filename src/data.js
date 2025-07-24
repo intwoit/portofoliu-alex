ee1c70801c2d2d449956dae07fb043d4d4a13f0d
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-25 01:33:50 +0300

    Update SEI: preț mediu și cantitate ajustate – 25 iulie 2025

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const investments = [
   { asset: "ETH", entries: [{ amount: 21579 / 2963, price: 2963 }] },
   { asset: "LINK", entries: [{ amount: 16400 / 17.50, price: 17.50 }] },
   { asset: "SOL", entries: [{ amount: 7327 / 181.91, price: 181.91 }] },
-  { asset: "SEI", entries: [{ amount: 1211 / 0.424, price: 0.424 }] },
+  { asset: "SEI", entries: [{ amount: 935 / 0.4317, price: 0.4317 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.755, price: 0.755 }] },
   { asset: "RNDR", entries: [{ amount: 2358 / 4.123, price: 4.123 }] },
   { asset: "LDO", entries: [{ amount: 945 / 1.1573, price: 1.1573 }] },
