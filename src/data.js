93a73d360cb69d49122c93f71f755dda90ecb38c
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-26 12:45:24 +0300

    Update portofoliu + cash for dips – 25 iulie 2025

--- src/data.js
+++ src/data.js
@@ -5,13 +5,6 @@ const investments = [
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 935 / 0.4317, price: 0.4317 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.755, price: 0.755 }] },
-  { asset: "RNDR", entries: [{ amount: 1158 / 4.123, price: 4.123 }] },
-  { asset: "LDO", entries: [{ amount: 945 / 1.1573, price: 1.1573 }] },
-  { asset: "GMX", entries: [{ amount: 300 / 13.22, price: 13.22 }] },
-  { asset: "STRK", entries: [{ amount: 480 / 0.137, price: 0.137 }] },
-  { asset: "RDNT", entries: [{ amount: 200 / 0.02478, price: 0.02478 }] },
-  { asset: "REZ", entries: [{ amount: 870 / 0.013497, price: 0.013497 }] },
-  { asset: "ETHFI", entries: [{ amount: 1500 / 1.257, price: 1.257 }] },
   { asset: "PENDLE", entries: [{ amount: 1795 / 4.106, price: 4.106 }] },
 ];
 
