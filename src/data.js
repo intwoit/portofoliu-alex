5bcac52d566685256dd694084f98f8562e30cc44
Alex Intwoit <intwoit.creative@gmail.com>
2025-07-31 08:45:48 +0300

    Updated data and CashForDips component

--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ const investments = [
   { asset: "LINK", entries: [{ amount: 20400 / 17.23, price: 17.23 }] },
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 935 / 0.4317, price: 0.4317 }] },
-  { asset: "FET", entries: [{ amount: 2800 / 0.7635, price: 0.7635 }] },
-  { asset: "PENDLE", entries: [{ amount: 2795 / 4.282, price: 4.282 }] },
+  { asset: "FET", entries: [{ amount: 1300 / 0.7635, price: 0.7635 }] },
+  { asset: "PENDLE", entries: [{ amount: 1295 / 4.282, price: 4.282  }] },
 ];
 
 export default investments;
