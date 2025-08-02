0fffe994ce900ac965295b54a2806980b709e9f1
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-02 23:17:39 +0300

    update: ETH buy 600 USDC on 2025-08-01, new avg and amount + updated cash for dips

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 32590 / 3165, price: 3165 }] },
+  { asset: "ETH", entries: [{ amount: 33190 / 3169, price: 3169 }] },
   { asset: "LINK", entries: [{ amount: 21200 / 17.20, price: 17.20 }] },
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
