ecdaf8ef54a54455e39efc9beb33a84315df2c60
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-02 23:02:53 +0300

    update: ETH, LINK, PENDLE, FET, SEI entries and cash for dips

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ function App() {
   Total P/L: ${format(totalProfit)} ({((totalProfit / totalInvestment) * 100).toFixed(2)}%)
 </p>
 
-        <p>Cash reserved for dips: $6830</p>
+        <p>Cash reserved for dips: $3680</p>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 31790 / 3155, price: 3155 }] },
-  { asset: "LINK", entries: [{ amount: 20400 / 17.23, price: 17.23 }] },
+  { asset: "ETH", entries: [{ amount: 32590 / 3165, price: 3165 }] },
+  { asset: "LINK", entries: [{ amount: 21200 / 17.20, price: 17.20 }] },
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
-  { asset: "SEI", entries: [{ amount: 935 / 0.4317, price: 0.4317 }] },
-  { asset: "FET", entries: [{ amount: 1300 / 0.7635, price: 0.7635 }] },
-  { asset: "PENDLE", entries: [{ amount: 1295 / 4.282, price: 4.282  }] },
+  { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
+  { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
+  { asset: "PENDLE", entries: [{ amount: 1795 / 4.182, price: 4.182  }] },
 ];
 
 export default investments;
