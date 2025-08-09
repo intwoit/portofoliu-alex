6816581f0bbbb3f835701a5d4850328394814630
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-09 11:03:09 +0300

    Update PENDLE amount and adjusted average price

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const investments = [
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
-  { asset: "PENDLE", entries: [{ amount: 1395 / 3.7506, price: 3.7506  }] },
+  { asset: "PENDLE", entries: [{ amount: 1395 / 3.6692, price: 3.6692  }] },
 ];
 
 export default investments;
