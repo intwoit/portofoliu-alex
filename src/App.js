450eaacc17cabc51110afb7bbb36651abda80c44
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-09 11:34:10 +0300

    Update ETH and LINK holdings after partial sell

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ function App() {
   Total P/L: ${format(totalProfit)} ({((totalProfit / totalInvestment) * 100).toFixed(2)}%)
 </p>
 
-        <p>Cash reserved for dips: $3186</p>
+        <p>Cash reserved for dips: $6223</p>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 33790 / 3172, price: 3172 }] },
-  { asset: "LINK", entries: [{ amount: 21500 / 17.17, price: 17.17 }] },
+  { asset: "ETH", entries: [{ amount: 32149 / 3134, price: 3134 }] },
+  { asset: "LINK", entries: [{ amount: 20105 / 16.97, price: 16.97 }] },
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
