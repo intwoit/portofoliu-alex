a9b9c4807e4f47fedb966a8855c760bb02530152
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-09 23:44:13 +0300

    Update ETH and LINK: new investment and average price

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
   { asset: "ETH", entries: [{ amount: 29149 / 3111, price: 3111 }] },
-  { asset: "LINK", entries: [{ amount: 19105 / 17.96, price: 17.96 }] },
+  { asset: "LINK", entries: [{ amount: 19105 / 17.07, price: 17.07 }] },
   { asset: "SOL", entries: [{ amount: 5662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
