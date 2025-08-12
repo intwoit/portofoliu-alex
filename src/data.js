2a52615134229961158cb9da5b98ab3d2a8ee65d
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-12 22:41:14 +0300

    Updated LINK, ETH, PENDLE investments and cash for dips

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 29149 / 3111, price: 3111 }] },
-  { asset: "LINK", entries: [{ amount: 22105 / 17.63, price: 17.63 }] },
+  { asset: "ETH", entries: [{ amount: 32149 / 3194, price: 3194 }] },
+  { asset: "LINK", entries: [{ amount: 24105 / 17.90, price: 17.90 }] },
   { asset: "SOL", entries: [{ amount: 2662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
-  { asset: "PENDLE", entries: [{ amount: 1395 / 3.6692, price: 3.6692  }] },
+  { asset: "PENDLE", entries: [{ amount: 2395 / 4.27, price: 4.27  }] },
 ];
 
 export default investments;
