8e23d4b32b9a362ca5261c1860196653f80a4bbb
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-17 21:02:06 +0300

    update LINK average price to 17.99

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
   { asset: "ETH", entries: [{ amount: 32149 / 3194, price: 3194 }] },
-  { asset: "LINK", entries: [{ amount: 28318 / 18.58, price: 18.58 }] },
+  { asset: "LINK", entries: [{ amount: 26177 / 17.99, price: 17.99 }] },
   { asset: "SOL", entries: [{ amount: 2662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
