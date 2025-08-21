8dc1dae7253bfa516cc44c1991b50427ce9a8911
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-22 00:48:46 +0300

    update project

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 32149 / 3194, price: 3194 }] },
-  { asset: "LINK", entries: [{ amount: 25345 / 17.99, price: 17.99 }] },
+  { asset: "ETH", entries: [{ amount: 34822 / 3256, price: 3256 }] },
+  { asset: "LINK", entries: [{ amount: 25645 / 18.04, price: 18.04 }] },
   { asset: "SOL", entries: [{ amount: 2662 / 181.91, price: 181.91 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
