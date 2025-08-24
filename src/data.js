1987fc01e10fdef11daaf80fc18db70bfaa91c20
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-24 15:52:23 +0300

    update portofoliu recalibrat

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 const investments = [
-  { asset: "BTC", entries: [{ amount: 17905 / 96057, price: 96057 }] },
-  { asset: "ETH", entries: [{ amount: 34822 / 3256, price: 3256 }] },
-  { asset: "LINK", entries: [{ amount: 25645 / 18.04, price: 18.04 }] },
-  { asset: "SOL", entries: [{ amount: 2662 / 181.91, price: 181.91 }] },
+  { asset: "BTC", entries: [{ amount: 15905 / 94150, price: 94150 }] },
+  { asset: "ETH", entries: [{ amount: 24822 / 3256, price: 2886 }] },
+  { asset: "LINK", entries: [{ amount: 20330 / 16.76, price: 16.76 }] },
+  { asset: "SOL", entries: [{ amount: 2062 / 175.7, price: 175.7 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
   { asset: "FET", entries: [{ amount: 1800 / 0.7315, price: 0.7315 }] },
-  { asset: "PENDLE", entries: [{ amount: 2395 / 4.27, price: 4.27  }] },
+  { asset: "PENDLE", entries: [{ amount: 1395 / 3.55, price: 3.55  }] },
 ];
 
 export default investments;
