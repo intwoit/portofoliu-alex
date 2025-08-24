3185bc191c56c1c0f743e6d21b6c6e4f13e9399e
Alex Intwoit <intwoit.creative@gmail.com>
2025-08-24 15:55:23 +0300

    update portofoliu recalibrat

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 15905 / 94150, price: 94150 }] },
-  { asset: "ETH", entries: [{ amount: 24822 / 3256, price: 2886 }] },
+  { asset: "ETH", entries: [{ amount: 24822 / 3256, price: 3256 }] },
   { asset: "LINK", entries: [{ amount: 20330 / 16.76, price: 16.76 }] },
   { asset: "SOL", entries: [{ amount: 2062 / 175.7, price: 175.7 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.3684, price: 0.3684 }] },
