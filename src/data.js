664c1b9147199715e8ecdde9cf708b8957239b0d
Alex Intwoit <intwoit.creative@gmail.com>
2025-09-23 08:59:29 +0300

    update portofoliu

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 20405 / 97725, price: 97725 }] },
   { asset: "ETH", entries: [{ amount: 9822 / 1907, price: 1907 }] },
-  { asset: "LINK", entries: [{ amount: 33218 / 19.58, price: 19.58 }] },
+  { asset: "LINK", entries: [{ amount: 33218 / 20.43, price: 20.43 }] },
   { asset: "SOL", entries: [{ amount: 5562 / 200.77, price: 200.77 }] },
   { asset: "SEI", entries: [{ amount: 1435 / 0.393, price: 0.393 }] },
   { asset: "FET", entries: [{ amount: 1857 / 0.825, price: 0.825 }] },
