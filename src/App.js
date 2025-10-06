bbb9a01621fe43014f773707355e2f497274c82f
Alex Intwoit <intwoit.creative@gmail.com>
2025-10-06 21:22:30 +0300

    update portofoliu

--- src/App.js
+++ src/App.js
@@ -143,7 +143,7 @@ function App() {
   Total P/L: ${format(totalProfit)} ({((totalProfit / totalInvestment) * 100).toFixed(2)}%)
 </p>
 
-        <p>Cash reserved for dips: $15340</p>
+        <p>Cash reserved for dips: $3340</p>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 const investments = [
   { asset: "BTC", entries: [{ amount: 32805 / 110002, price: 110002 }] },
-  { asset: "ETH", entries: [{ amount: 15268.30 / 2329.90, price: 2329.90 }] },
+  { asset: "ETH", entries: [{ amount: 25268.30 / 2892, price: 2892 }] },
   { asset: "LINK", entries: [{ amount: 17199 / 20.24, price: 20.24 }] },
-  { asset: "SOL", entries: [{ amount: 4208 / 200.77, price: 200.77 }] },
+  { asset: "SOL", entries: [{ amount: 6208 / 210.40, price: 210.40 }] },
   { asset: "SEI", entries: [{ amount: 505 / 0.393, price: 0.393 }] },
   { asset: "FET", entries: [{ amount: 740 / 0.825, price: 0.825 }] },
   { asset: "PENDLE", entries: [{ amount: 4000 / 4.52, price: 4.52  }] },
